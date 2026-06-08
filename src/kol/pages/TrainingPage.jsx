e3f4d8d04987a33a92dcb779e8949186bbc354a2
Zackary Nisbet <55851584+ZackaryNisbet@users.noreply.github.com>
2026-06-08 03:23:33 +0000

    refactor(training): extract TrainingHistoryStatusChange leaf component

--- src/kol/pages/TrainingPage.jsx
+++ src/kol/pages/TrainingPage.jsx
@@ -103,6 +103,7 @@ import PerformanceReviewComplianceGrid, {
   PerformanceReviewComplianceGridStyles,
   ReviewCycleCell,
 } from "../components/PerformanceReviewComplianceGrid";
+import { TrainingHistoryStatusChange } from "./training/components/TrainingHistoryStatusChange";
 import { CompactProgressBar } from "./training/components/CompactProgressBar";
 import { ProgressBar } from "./training/components/ProgressBar";
 import { HourAnalysisPositionMoveControl } from "./training/components/HourAnalysisPositionMoveControl";
@@ -1316,46 +1317,6 @@ function getReadinessCellActorLine(cell = {}, status = "") {
   return "";
 }
 
-function TrainingHistoryStatusChange({ statusChange }) {
-  if (!statusChange?.nextLabel) return null;
-  const nextStyle = PCT_READINESS_STATUS_STYLES[statusChange.nextStatus] || PCT_READINESS_STATUS_STYLES.not_started;
-  const previousStyle = PCT_READINESS_STATUS_STYLES[statusChange.previousStatus] || PCT_READINESS_STATUS_STYLES.not_started;
-  return (
-    <div style={{ marginTop: 7, display: "flex", alignItems: "center", gap: 6, flexWrap: "wrap" }}>
-      {statusChange.previousLabel ? (
-        <span style={{
-          display: "inline-flex",
-          alignItems: "center",
-          borderRadius: 999,
-          border: `1px solid ${previousStyle.border}`,
-          background: previousStyle.bg,
-          color: previousStyle.text,
-          padding: "2px 7px",
-          fontSize: 10.5,
-          fontWeight: 900,
-          textDecoration: "line-through",
-          opacity: 0.72,
-        }}>
-          {statusChange.previousLabel}
-        </span>
-      ) : null}
-      {statusChange.previousLabel ? <span style={{ color: C.textMut, fontSize: 11, fontWeight: 900 }}>-&gt;</span> : null}
-      <span style={{
-        display: "inline-flex",
-        alignItems: "center",
-        borderRadius: 999,
-        border: `1px solid ${nextStyle.border}`,
-        background: nextStyle.bg,
-        color: nextStyle.text,
-        padding: "2px 7px",
-        fontSize: 10.5,
-        fontWeight: 950,
-      }}>
-        {statusChange.nextLabel}
-      </span>
-    </div>
-  );
-}
 
 
 
